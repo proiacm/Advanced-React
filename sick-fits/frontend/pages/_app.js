531d93a44a7fe4d7772351d11b55ddc993f03ed5
CiaraMaria <proiacm@gmail.com>
2021-01-19 15:36:48 -0700

    Update _app.js

--- sick-fits/frontend/pages/_app.js
+++ sick-fits/frontend/pages/_app.js
@@ -1,4 +1,5 @@
 import App, { Container } from 'next/app';
+import Page from '../components/Page';
 
 class MyApp extends App {
 
@@ -7,7 +8,9 @@ class MyApp extends App {
 
     return (
       <Container>
-        <Component />
+        <Page>
+          <Component />
+        </Page>
       </Container>
     );
   }
