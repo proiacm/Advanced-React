caf6ca8f4c3b66d1b3e582689e72ce097293a14a
CiaraMaria <proiacm@gmail.com>
2021-01-29 10:26:38 -0700

    Apollo set up

---
+++ sick-fits/frontend/components/Item.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class Item extends Component {
+  static propTypes = {
+    item: PropTypes.object.isRequired
+  }
+
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default Item;
